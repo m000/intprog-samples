aa539bb3f2211ab081ec255a10cd4914e33186d5
Manolis Stamatogiannakis <manolis.stamatogiannakis@vu.nl>
2015-10-10 16:25:31 +0200

    Fixing template.

--- assignment4/web/index.php
+++ assignment4/web/index.php
@@ -1,7 +1,8 @@
 <?php
 $topdir=realpath(".");
-
 include('smarty/Smarty.class.php');
+include('config.php');
+
 $smarty = new Smarty;
 $smarty->setTemplateDir($topdir.'/smarty/templates');
 $smarty->setCompileDir($topdir.'/smarty/templates_c');
@@ -9,7 +10,12 @@ $smarty->setCacheDir($topdir.'/smarty/cache');
 $smarty->setConfigDir($topdir.'/smarty/configs');
 
 $smarty->display('tpl/index.html');
-
+// $WEB_BASEPHP="http://acropolis.cs.vu.nl/~spyros/intprog/";
+// $WEB_BASECGI="http://mymachine.cs.vu.nl:8001/cgi-bin/";
+// $HOTEL_ADDRESS="localhost";
+// $HOTEL_PORT=3211;
+// $PAPER_ADDRESS="localhost";
+// $PAPER_PORT=3222;
 
 
 ?>
