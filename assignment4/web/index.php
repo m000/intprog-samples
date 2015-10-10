5eea543a6ae3a8841fdfd297bf3e1877deafe51c
Manolis Stamatogiannakis <manolis.stamatogiannakis@vu.nl>
2015-10-10 02:12:19 +0200

    skeleton fixes

---
+++ assignment4/web/index.php
@@ -0,0 +1,15 @@
+<?php
+$topdir=realpath(".");
+
+include('smarty/Smarty.class.php');
+$smarty = new Smarty;
+$smarty->setTemplateDir($topdir.'/smarty/templates');
+$smarty->setCompileDir($topdir.'/smarty/templates_c');
+$smarty->setCacheDir($topdir.'/smarty/cache');
+$smarty->setConfigDir($topdir.'/smarty/configs');
+
+$smarty->display('tpl/index.html');
+
+
+
+?>
